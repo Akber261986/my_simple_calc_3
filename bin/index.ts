6ab13c0a66d67220ee1cff09a825a8e20387e3ca
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2024-04-05 20:49:25 +0500

    Some little changes

--- bin/index.ts
+++ bin/index.ts
@@ -3,22 +3,11 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-    {
-        name: "no1",
-        type: "number",
-        message: "Your First Number"
-    },
-    {
-        name: "no2",
-        type: "number",
-        message: "Your Second Number"
-    },
-    {
-        name: "oprator",
-        type: "list",
-        message: "Slect Your Oprator",
-        choices: ['add', 'sub', 'mult', 'div']
-    }
+    {name: "no1",type: "number",message: "Your First Number"},
+
+    {name: "no2",type: "number",message: "Your Second Number"},
+
+    {name: "oprator",type: "list",message: "Slect Your Oprator",choices: ['add', 'sub', 'mult', 'div']}
 ])
 if ( answer.oprator === 'add'){
     console.log(answer.no1+answer.no2);
